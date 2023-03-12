a9c133a5f451d9bf81f19a0465204c51c9c48d6c
wadaka0821 <wadaka0821@gmail.com>
2023-03-12 21:04:16 +0900

    bug fixed

--- model.py
+++ model.py
@@ -28,14 +28,13 @@ class Generator():
     def generate(self, 
                  prefix:str, 
                  max_length:int = 64,
-                 top_k = 5) -> str:
+                 top_k = 3) -> str:
         tokenized_prefix = self.tokenizer(prefix, return_tensors='pt').to(self.device)
         generated_ids = tokenized_prefix.input_ids[0, 1:-1].tolist()
         max_length = max_length + len(generated_ids)
-        while len(generated_ids) < max_length and generated_ids[-1] != self.mask_token_id:
+        while len(generated_ids) < max_length and generated_ids[-1] != self.eos_token_id:
             output = self.mlm(input_ids=torch.Tensor([generated_ids+[self.mask_token_id]]).long().to(self.device))
             generated_id = torch.topk(output.logits[0, -1], k=top_k).indices[torch.randint(low=0, high=top_k-1, size=(1,))]
-            generated_id = torch.argmax(output.logits[0, -1])
             generated_ids.append(generated_id.item())
         decoded_text = self.tokenizer.decode(generated_ids)
 
@@ -48,3 +47,4 @@ if __name__ == '__main__':
             'Below you will find some great videos that will encourage you, train you and build you up in hearing from GOD and being able to let HIM fulfill HIS plan in your life. SOMETHING NEW THAT WILL HELP YOU HEAR GODS VOICE! How to']
     for t in text:
         print(generator.generate(t))
+        print()
