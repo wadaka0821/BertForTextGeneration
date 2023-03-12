7210795b96f483cbe7cb5bcc26a5d64a8c744e71
wadaka0821 <wadaka0821@gmail.com>
2023-03-12 21:36:39 +0900

    add multiple <mask> tokens for better text

--- model.py
+++ model.py
@@ -10,7 +10,7 @@ class Generator():
     def __init__(self, 
                  model_name:str ="google/bert_uncased_L-12_H-768_A-12", 
                  device:DEVICE = 'cpu', 
-                 eos_token:str = '[SEP]',
+                 eos_tokens:list[str] = ['[SEP]', '.'],
                  mask_token:str = '[MASK]') -> None:
         self.model_name = model_name
         try:
@@ -21,30 +21,31 @@ class Generator():
             sys.exit(1)
         self.tokenizer = AutoTokenizer.from_pretrained(model_name)
         self.device = device
-        self.eos_token_id = self.tokenizer.encode(eos_token)[1]
+        self.eos_token_ids = [self.tokenizer.encode(t)[1] for t in eos_tokens]
         self.mask_token_id = self.tokenizer.encode(mask_token)[1]
 
     @torch.inference_mode()
     def generate(self, 
                  prefix:str, 
                  max_length:int = 64,
-                 top_k = 3) -> str:
+                 top_k = 10,
+                 mask_length = 4) -> str:
         tokenized_prefix = self.tokenizer(prefix, return_tensors='pt').to(self.device)
         generated_ids = tokenized_prefix.input_ids[0, 1:-1].tolist()
         max_length = max_length + len(generated_ids)
-        while len(generated_ids) < max_length and generated_ids[-1] != self.eos_token_id:
-            output = self.mlm(input_ids=torch.Tensor([generated_ids+[self.mask_token_id]]).long().to(self.device))
-            generated_id = torch.topk(output.logits[0, -1], k=top_k).indices[torch.randint(low=0, high=top_k-1, size=(1,))]
+        while len(generated_ids) < max_length and generated_ids[-1] not in self.eos_token_ids:
+            output = self.mlm(input_ids=torch.Tensor([generated_ids+[self.mask_token_id for _ in range(mask_length)]]).long().to(self.device))
+            generated_id = torch.topk(output.logits[0, -mask_length], k=top_k).indices[torch.randint(low=0, high=top_k-1, size=(1,))]
             generated_ids.append(generated_id.item())
         decoded_text = self.tokenizer.decode(generated_ids)
 
         return decoded_text
 
 if __name__ == '__main__':
-    generator = Generator()
+    generator = Generator(model_name="xlm-roberta-base", eos_tokens=['</s>', '.'], mask_token='<mask>')
     text = ['The man eats',
             'A bird is',
-            'Below you will find some great videos that will encourage you, train you and build you up in hearing from GOD and being able to let HIM fulfill HIS plan in your life. SOMETHING NEW THAT WILL HELP YOU HEAR GODS VOICE! How to']
+            'My name is Clara and I am']
     for t in text:
         print(generator.generate(t))
         print()
